aaa779f8ed8fbcfd3e910586f9fb49ce0f12fbcd
FAZLA RABBY <108793623+FazlarabbyWD@users.noreply.github.com>
2023-02-06 00:36:24 +0600

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,14 @@
+<?php
+$tutionFee = 29999  ;//enter the tution fee 
+$commission = null;
+
+$commission = ($tutionFee >= 20000) ? ($tutionFee * 0.25) : 
+             (($tutionFee >= 10000 && $tutionFee < 20000) ? ($tutionFee * 0.20) : 
+             (($tutionFee < 10000 && $tutionFee >= 7000) ? ($tutionFee * 0.15) : 
+             ($tutionFee < 7000 ? "Invalid" : '')));
+
+echo "The commission is: $commission";
+
+
+
+
